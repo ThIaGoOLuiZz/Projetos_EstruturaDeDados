d6a19efc13ca8e2c3079fc23c2562af3883758e3
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-03-11 21:15:20 -0300

    Adicionado projeto de Torre de Hanói com recursividade

---
+++ Trilha_03/Exercicio_06/Exercicio_06/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio_06
+{
+    internal class Program
+    {
+        static int contadorMovimentos = 0;
+
+        static void MoverDiscos(int n, char origem, char destino, char auxiliar)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine($"Mover disco 1 de {origem} para {destino}");
+                contadorMovimentos++;
+                return;
+            }
+
+            MoverDiscos(n - 1, origem, auxiliar, destino);
+            Console.WriteLine($"Mover disco {n} de {origem} para {destino}");
+            contadorMovimentos++;
+            MoverDiscos(n - 1, auxiliar, destino, origem);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de discos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSolução da Torre de Hanói:");
+            MoverDiscos(n, 'A', 'C', 'B');
+
+            Console.WriteLine($"\nNúmero total de movimentos: {contadorMovimentos}");
+        }
+    }
+}
