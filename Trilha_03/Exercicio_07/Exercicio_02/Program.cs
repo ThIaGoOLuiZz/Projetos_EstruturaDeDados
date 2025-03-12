b54c678b5c6ace48ffb96ebce4d092003202d6cc
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-03-11 21:20:46 -0300

    Adicionado projeto de contagem de nodes

---
+++ Trilha_03/Exercicio_07/Exercicio_02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaEncadeada lista = new ListaEncadeada();
+
+            lista.InsertAtEnd(10);
+            lista.InsertAtEnd(20);
+            lista.InsertAtEnd(30);
+
+
+            Console.WriteLine("Número de nós na lista: " + lista.ContaNodes(lista.head));
+        }
+    }
+}
